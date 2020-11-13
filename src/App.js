6510dcfff25ebbc2f12dd02494e118120472bbbf
Daniel Bemsen <54921134+AugustBemsen@users.noreply.github.com>
2020-11-13 02:31:28 +0100

    commit test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./styles.css";
 
-
 const App = () => {
+  //  Set up state
+
+  const [myName, setMyName] = useState("");
+
+  //  Set up Ref
+
+  // const { current } = useRef("");
+
+  const preName = useRef("");
+  const oldName = useRef("");
+  useEffect(() => {
+    preName.current = myName;
+  }, [myName]);
+
+  const getInput = (e) => setMyName(e.target.value);
+
   return (
     <div className="App">
-      
+      <input value={myName} onChange={getInput} />
+      <p>{`My name is ${myName} , and it used to be ${preName.current}`}</p>
     </div>
   );
-}
+};
 
 export default App;
